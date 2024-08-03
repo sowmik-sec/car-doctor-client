4edd585c1cf082b56301d8bf3b03b295b501a00f
sowmik-sec <sowmik.sec@gmail.com>
2024-08-03 21:26:46 +0600

    project initialization

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+      },
+    ],
+  },
+]);
+
+export default router;
