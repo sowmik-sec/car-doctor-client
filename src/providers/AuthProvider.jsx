cd1431d50330036d26487cc280f073f0c9551990
sowmik-sec <sowmik.sec@gmail.com>
2024-08-04 11:23:20 +0600

    create user with email and password

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,17 @@
 import { createContext, useState } from "react";
+import auth from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const AuthContext = createContext();
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const authInfo = {};
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { user, loading, createUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
