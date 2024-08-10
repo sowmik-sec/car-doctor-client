f7e779f2aac39cd252c233a2a791c0730140b73d
sowmik-sec <sowmik.sec@gmail.com>
2024-08-10 12:46:37 +0600

    handleBookingConfirm

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -31,7 +31,27 @@ function Bookings() {
         });
     }
   };
-  const handleBookingConfirm = () => {};
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          // update state
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+        }
+      });
+  };
   return (
     <div>
       <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
