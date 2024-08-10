5295f10cc7dfdc3781b1268ca29398b82b790945
sowmik-sec <sowmik.sec@gmail.com>
2024-08-10 11:51:11 +0600

    delete unnecessary component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,6 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/login/Login";
 import Signup from "../pages/Signup/Signup";
-import Checkout from "../pages/Checkout/Checkout";
 import BookService from "../pages/BookService/BookService";
 
 const router = createBrowserRouter([
@@ -23,12 +22,7 @@ const router = createBrowserRouter([
         path: "/signup",
         element: <Signup />,
       },
-      {
-        path: "checkout/:id",
-        element: <Checkout />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/services/${params.id}`),
-      },
+
       {
         path: "book/:id",
         element: <BookService />,
