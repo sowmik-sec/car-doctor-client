f5a96593b30e30eb7e50adc631b635af4a14473f
sowmik-sec <sowmik.sec@gmail.com>
2024-08-10 12:37:56 +0600

    create private route

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+function PrivateRoute({ children }) {
+  const { user, loading } = useContext(AuthContext);
+  if (loading)
+    return (
+      <div className="flex justify-center items-center">
+        <span className="loading loading-spinner text-success loading-lg"></span>
+      </div>
+    );
+  if (!user) return <Navigate to={"/login"} replace />;
+  return children;
+}
+
+export default PrivateRoute;
