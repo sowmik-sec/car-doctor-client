7f9899cd28e2aa72cd6307186855aa78b89fc353
sowmik-sec <sowmik.sec@gmail.com>
2024-08-10 10:32:14 +0600

    take user to checkout page

--- src/pages/Home/Services/Service.jsx
+++ src/pages/Home/Services/Service.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Service({ service }) {
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="mx-auto shadow-md p-3 rounded-md">
       <img src={img} alt="" />
       <h3 className="text-2xl font-bold">{title}</h3>
       <div className="flex justify-between">
         <p className="text-xl text-p-color">Price: ${price}</p>
-        <FaArrowRight className="text-p-color" />
+        <Link to={`/checkout/${_id}`}>
+          <FaArrowRight className="text-p-color" />
+        </Link>
       </div>
     </div>
   );
