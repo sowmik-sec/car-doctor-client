a05b7c943fd26083bdc41b31cef6ba47ce7fd952
sowmik-sec <sowmik.sec@gmail.com>
2024-08-11 22:17:55 +0600

    Send token server to client and client to the server side

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,18 +1,22 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import axios from "axios";
 
 function Bookings() {
   const { user } = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
   const url = `http://localhost:5000/bookings?email=${user.email}`;
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBookings(data);
-      });
+    axios.get(url, { withCredentials: true }).then((res) => {
+      setBookings(res.data);
+    });
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setBookings(data);
+    //   });
   }, [url]);
   const handleDelete = (id) => {
     const proceed = confirm("Are You sure you want to delete");
